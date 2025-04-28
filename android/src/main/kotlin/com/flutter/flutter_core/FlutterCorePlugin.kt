5e3b06f7ca39b621eebc99c549435c30d970e17f
sinanhaci <sinan.hacia@gmail.com>
2025-04-28 17:27:02 +0300

    Add Android ID
    Add Android ID retrieval and update device info interface  - Implement `getAndroidId` method in `FlutterCorePlugin` to retrieve the Android ID. - Update `ICoreDeviceInfo` interface to include `deviceIdentifier`. - Refactor `CoreDeviceInfo` to use `deviceIdentifier` instead of `deviceName`. - Add `.cxx` directory to `.gitignore` files in both root and example Android directories.

--- android/src/main/kotlin/com/flutter/flutter_core/FlutterCorePlugin.kt
+++ android/src/main/kotlin/com/flutter/flutter_core/FlutterCorePlugin.kt
@@ -3,12 +3,16 @@ package com.flutter.flutter_core
 import androidx.annotation.NonNull
 import com.huawei.hms.api.HuaweiApiAvailability
 import android.content.Context
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.provider.Settings
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+
 
 /** FlutterCorePlugin */
 class FlutterCorePlugin: FlutterPlugin, MethodCallHandler {
@@ -18,8 +22,10 @@ class FlutterCorePlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var contentResolver: ContentResolver
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    contentResolver = flutterPluginBinding.applicationContext.contentResolver
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_core")
     channel.setMethodCallHandler(this)
     mContext = flutterPluginBinding.applicationContext
@@ -34,15 +40,27 @@ class FlutterCorePlugin: FlutterPlugin, MethodCallHandler {
       val sdkVersion = android.os.Build.VERSION.SDK_INT
       val availability = HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(mContext!!,sdkVersion)
       result.success(availability)
+    }else if (call.method == "getAndroidId") {
+      try {
+        result.success(getAndroidId())
+      } catch (e: Exception) {
+        result.error("ERROR_GETTING_ID", "Failed to get Android ID", e.localizedMessage)
+      }
     }
     else {
       result.notImplemented()
     }
   }
 
+  @SuppressLint("HardwareIds")
+  private fun getAndroidId(): String? {
+    return Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+  }
+
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
     mContext = null
   }
+
 
 }
